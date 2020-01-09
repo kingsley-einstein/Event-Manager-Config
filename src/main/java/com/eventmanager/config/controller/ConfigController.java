0505c3ae0c960cd1344c15cbc4617c17f7eda870
Kingsley Victor <javaprodigy56@gmail.com>
2020-01-09 21:04:00 +0100

    Comment code

--- src/main/java/com/eventmanager/config/controller/ConfigController.java
+++ src/main/java/com/eventmanager/config/controller/ConfigController.java
@@ -1,23 +1,23 @@
-package com.eventmanager.config.controller;
+// package com.eventmanager.config.controller;
 
-import java.util.HashMap;
-import java.util.Map;
+// import java.util.HashMap;
+// import java.util.Map;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-// For Kintohub health check
-@RequestMapping("/")
-@RestController
-public class ConfigController {
-  @GetMapping
-  public ResponseEntity<Map<String, Object>> ping() {
-    Map<String, Object> map = new HashMap<>();
-    map.put("statusCode", 200);
-    map.put("body", "This is the Event Manager configuration server");
-    return new ResponseEntity<>(map, HttpStatus.OK);
-  }
-}
+// // For Kintohub health check
+// @RequestMapping("/")
+// @RestController
+// public class ConfigController {
+//   @GetMapping
+//   public ResponseEntity<Map<String, Object>> ping() {
+//     Map<String, Object> map = new HashMap<>();
+//     map.put("statusCode", 200);
+//     map.put("body", "This is the Event Manager configuration server");
+//     return new ResponseEntity<>(map, HttpStatus.OK);
+//   }
+// }
